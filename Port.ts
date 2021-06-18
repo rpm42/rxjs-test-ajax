7f5ec0a9999e27448aa23280de841c24bf117477
rpm42 <rpm42@ya.ru>
2021-06-18 10:07:14 +0300

    Initial commit

---
+++ Port.ts
@@ -0,0 +1,44 @@
+import { of, throwError, Subject, from } from 'rxjs'
+import {
+  tap,
+  delay,
+  concatMap,
+  catchError,
+  mergeMap,
+  mapTo
+} from 'rxjs/operators'
+import Irp from './Irp'
+import { fromFetch } from 'rxjs/fetch'
+import { ajax } from 'rxjs/ajax'
+
+export default class Port {
+  dispatch = (irp: Irp) => {
+    console.log('port', irp)
+    return ajax({
+      url: irp.req.url,
+      method: irp.req.method
+    }).pipe(
+      mergeMap(resp => {
+        console.log('resp', resp)
+        // irp.ok = resp.ok
+        irp.res.status = resp.status
+        return from(resp.text()).pipe(tap(v => console.log('get body 1', v)))
+      }),
+      tap(v => console.log('get body 2', v)),
+      mapTo(irp),
+      catchError(e => {
+        console.log('catchError', e, irp)
+        irp.error(e.message)
+        return of(irp)
+      })
+    )
+  }
+
+  irpIn$ = new Subject<Irp>()
+  irpOut$ = this.irpIn$.pipe(mergeMap(this.dispatch))
+
+  constructor(next$: Subject<Irp>, back$: Subject<Irp>) {
+    next$.subscribe(this.irpIn$)
+    this.irpOut$.subscribe(back$)
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-fk6xrt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-fk6xrt)
